2d8720ff414298179f18cfbbafa5361cf2b2c71d
cmgau <82530248+cmgau@users.noreply.github.com>
2021-10-04 08:39:19 +1100

    Adjustments

--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
-const filter_btns = document.querySelectorAll(".filter-btn");
-const skills_wrap = document.querySelector(".skills");
-const skills_bars = document.querySelectorAll(".skill-progress");
-const records_wrap = document.querySelector(".records");
-const records_numbers = document.querySelectorAll(".number");
-const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
-
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
-});
-
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-});
 
 function closeMenu() {
   navbar.classList.remove("open");
